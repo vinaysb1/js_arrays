8edc227da786ba234d58a36b2cce571d7b4d7de0
vinay <vinaykumarbeelagi@gmail.com>
2022-11-23 20:28:26 +0530

    array basics

---
+++ index.js
@@ -0,0 +1,24 @@
+const cars = ["volvo","benz","jeep"];
+console.log (cars[0]);
+console.log (cars[1]);
+console.log (cars[2]);
+
+const number = [1,2,3,4,5];
+for (i=0;i<number.length;i++)
+{
+    console.log(array[i]);
+}
+
+index = 0;
+array1 = [2,4,6,8,10];
+while(index<array1.length);
+{
+console.log(array[index]);
+index++;
+}
+
+const num1 = [2,4,6,8];
+function multiply (item){
+    return item*10
+}
+console.log(num1.map(multiply));
