b1a28518470e7fa1561b2a3ba320313db79cc54f
vinay <vinaykumarbeelagi@gmail.com>
2022-11-24 11:39:33 +0530

    arrays for of

--- index.js
+++ index.js
@@ -9,54 +9,54 @@ for (i=0;i<number.length;i++)
 console.log(number[i]);
 }
 
-// index = 0;
-// array1 = [2,4,6,8,10];
-// while(index<array1.length);
-// {
-// console.log(array1[index]);
-// index++;
-// }
+index = 0;
+array1 = [2,4,6,8,10];
+while(index<array1.length);
+{
+console.log(array[index]);
+index++;
+}
 
-// const num1 = [2,4,6,8];
-// function multiply (item){
-//     return item*10
-// }
-// console.log(num1.map(multiply));
+const num1 = [2,4,6,8];
+function multiply (item){
+    return item*10
+}
+console.log(num1.map(multiply));
 
-// var myArray = [
-//     {id: 1, name: 'John', age: 12},
-//     {id: 2, name: 'Jane', age: 14},
-//     {id: 3, name: 'Martin', age: 13},
-//     {id: 4, name: 'Katie', age: 17},
-//     {id: 5, name: 'Louis', age: 11}
-// ];
+var myArray = [
+    {id: 1, name: 'John', age: 12},
+    {id: 2, name: 'Jane', age: 14},
+    {id: 3, name: 'Martin', age: 13},
+    {id: 4, name: 'Katie', age: 17},
+    {id: 5, name: 'Louis', age: 11}
+];
 
-// for (i=0;i<myArray.length;i++) {
-//     console.log(myArray[i])
-//     console.log(profile.name)
-// }
+for (i=0;i<myArray.length;i++) {
+    console.log(myArray[i])
+    console.log(profile.name)
+}
 
-// const num3 = [2,4,6,8,10];
-// for (let numbers of array num3){
-//     console.log(num3);
-// }
+const num3 = [2,4,6,8,10];
+for (let numbers1 of  num3){
+    console.log(numbers1);
+}
 
-//  const num4 = [1,2,3,4,6,7];
-//  i=0;
-//  do {
-//     console.log(num4[i])
-//     i++;
-//  }
-// while (i<num4.length);
+ const num4 = [1,2,3,4,6,7];
+ i=0;
+ do {
+    console.log(num4[i])
+    i++;
+ }
+while (i<num4.length);
 
-// var myArray1 = [
-//     {id: 1, firstName: 'John', lastName: 'Smith', age: 12},
-//     {id: 2, firstName: 'Jane', lastName: 'Brown', age: 14},
-//     {id: 3, firstName: 'Martin', lastName: 'Johnson', age: 13},
-//     {id: 4, firstName: 'Katie', lastName: 'Miller', age: 17},
-//     {id: 5, firstName: 'Louis', lastName: 'Wilson', age: 11}
-// ];
+var myArray1 = [
+    {id: 1, firstName: 'John', lastName: 'Smith', age: 12},
+    {id: 2, firstName: 'Jane', lastName: 'Brown', age: 14},
+    {id: 3, firstName: 'Martin', lastName: 'Johnson', age: 13},
+    {id: 4, firstName: 'Katie', lastName: 'Miller', age: 17},
+    {id: 5, firstName: 'Louis', lastName: 'Wilson', age: 11}
+];
 
-// function fullname(item){
+function fullname(item){
     
-// }
+}
